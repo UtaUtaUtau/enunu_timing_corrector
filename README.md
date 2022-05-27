e64fa16d7ae11b17d1d61f0a025195d13cc0c467
UtaUtaUtau <jmctayag@gmail.com>
2022-05-27 21:23:00 +0800

    Release it to the world
    Hey :)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # enunu_timing_corrector
- General timing corrector for ENUNU
+General timing corrector for ENUNU.
+
+# How to Use
+1. Paste the timing_corrector folder into the voicebank of choice.
+2. Append the text written in `enuconfig.yaml` at the end of the existing `enuconfig.yaml` of the voicebank.
+	- If the voicebank in question already has this part in its `enuconfig.yaml`, you may edit the `timing_editor` section to be similar to the given `enuconfig.yaml`.
+3. Change the `vowels.txt` to list out the vowels of the language of the voicebank comma-separated (e.g. `a,i,u,e,o,A,I,U,E,O,N`, `aa,ae,ah,ao,aw,ax,ay,eh,er,ey,ih,iy,ow,oy,uh,uw`)
+
+# Remarks
+This was only a quick test run of timing correction for ENUNU. There might be a potential issue where a part of the corrected label might have its end be earlier than the start, but that's all there is to its issues. The consonant lengths are based off of the timing that ENUNU generates by itself. I'm not sure if `velocity_applier` (the other script that comes with canon's timing corrector in Ritsu NNSVS) works with other languages, but if it does, it may be used to shorten consonants and decreasing the chances of the said issue.
+
+That being said, I also think it's generally better to use canon's timing corrector for Japanese voices instead of this. This was made to hopefully support all languages.
---
+++ timing_corrector/script.py
@@ -0,0 +1,142 @@
+from argparse import ArgumentParser
+import os
+
+class Phoneme(): # Phoneme Class
+    def __init__(self, start, end, phone):
+        self.start = start
+        self.end = end
+        self.phone = phone
+        self.duration = end - start
+
+    @staticmethod
+    def from_line(line):
+        l = line.strip().split()
+        return Phoneme(int(l[0]), int(l[1]), l[2])
+
+    def __eq__(self, other):
+        return (self.start == other.start) and (self.end == other.end)
+
+    def __repr__(self):
+        return f'{self.start} {self.end} {self.phone}'
+
+    def __str__(self):
+        return self.__repr__()
+
+def parse_labels(mono_score, mono_timing):
+    '''
+    Parses labels and groups phonemes by note by checking the mono score label
+    '''
+    # Read labels
+    score = open(mono_score, encoding='utf8')
+    timing = open(mono_timing, encoding='utf8')
+
+    # Prepare list
+    score_list = []
+    timing_list = []
+
+    # Start reading
+    score_line = score.readline()
+    timing_line = timing.readline()
+    while score_line:
+        # Make phoneme class from line
+        score_phone = Phoneme.from_line(score_line)
+        timing_phone = Phoneme.from_line(timing_line)
+
+        if len(score_list) == 0: # Start edge case
+            score_list.append([score_phone])
+            timing_list.append([timing_phone])
+        elif score_list[-1][-1] == score_phone: # Append to last list if current mono score has the same timing as the first
+            score_list[-1].append(score_phone)
+            timing_list[-1].append(timing_phone)
+        else:
+            score_list.append([score_phone])
+            timing_list.append([timing_phone])
+        
+        score_line = score.readline()
+        timing_line = timing.readline()
+
+    # Return both lists
+    return score_list, timing_list
+
+def correct_timing(score_list, timing_list, vowels):
+    '''
+    Correct timing by referencing durations from timing list.
+    The score list will be the corrected version of the timing list.
+    '''
+    for i in range(len(score_list)): # Initial correct (note by note)
+        score = score_list[i]
+        timing = timing_list[i]
+        if len(score) != 1: # No need to correct notes with only one phoneme inside
+            # Find vowel
+            vowel_idx = 0
+            for j in range(len(score)):
+                if score[j].phone in vowels:
+                    vowel_idx = j
+                    break
+
+            # Correct onset positioning
+            for j in range(vowel_idx-1, -1, -1):
+                score[j].end = score[j+1].start
+                score[j].start = score[j].end - timing[j].duration
+                score[j].duration = timing[j].duration
+
+            # Correct coda
+            coda_len = 0
+            for j in range(vowel_idx+1, len(score)):
+                # Accumulate coda length...
+                coda_len += timing[j].duration
+
+            # ...and subtract to vowel
+            score[vowel_idx].duration -= coda_len
+            score[vowel_idx].end -= coda_len
+
+            # Correct coda positioning
+            for j in range(vowel_idx+1, len(score)):
+                score[j].start = score[j-1].end
+                score[j].end = score[j].start + timing[j].duration
+                score[j].duration = timing[j].duration
+
+    for i in range(1, len(score_list)): # Overlapping label correct
+        # TODO: Remove the possibility of end being equal or earlier than start
+        curr = score_list[i]
+        prev = score_list[i-1]
+        if curr[0].start != prev[-1].end: # If overlapping
+            # Correct end of previous note
+            prev[-1].end = curr[0].start
+            if len(prev) == 1 or prev[-1].phone in vowels:
+                # Skip if previous note only has one phoneme or the last phoneme is a vowel
+                continue
+            # Correct coda positioning
+            prev[-1].start = curr[0].start - prev[-1].duration
+            for j in range(len(prev)-2, -1, -1):
+                prev[j].end = prev[j+1].start
+                if prev[j].phone in vowels:
+                    # Stop if a vowel is seen. Putting vowels in one note is now... kind of bad to do from this.
+                    break
+                prev[j].start = prev[j].end - prev[j].duration
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Corrects timing from mono score and generated timing')
+    parser.add_argument('--mono_score', help='The mono score of the output')
+    parser.add_argument('--mono_timing', help='The processed timing')
+
+    args, _ = parser.parse_known_args()
+
+    vowels = open('vowels.txt', encoding='utf8').read().split(',')
+    #print(vowels)
+
+    score_list, timing_list = parse_labels(args.mono_score, args.mono_timing)
+    #print(score_list[0:5])
+    #print(timing_list[0:5])
+
+    correct_timing(score_list, timing_list, vowels)
+    #print(score_list[0:5])
+    #print(timing_list[0:5])
+
+    #os.rename(args.mono_timing, args.mono_timing + '.bak')
+
+    # Write corrected label to timing
+    with open(args.mono_timing, 'w', encoding='utf8', newline='\n') as f:
+        for i in score_list:
+            for j in i:
+                f.write(str(j) + '\n')
